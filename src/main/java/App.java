9540e623d3ae8439d940b0cd28421ca5e16a5c8b
dhruthi-am <dhruthiam@gmail.com>
2026-04-17 15:00:19 +0530

    second

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "1BI23CS125 Manasvi J";
     }
 
     public static void main(String[] args) {
